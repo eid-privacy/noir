90611fbca832150a86536b6b32b6005a550293ef
Clement Humbert <clement.humbert@sicpa.com>
2026-03-17 13:14:44 +0100

    add T-256

--- acvm-repo/acir_field/src/lib.rs
+++ acvm-repo/acir_field/src/lib.rs
@@ -28,8 +28,10 @@ pub fn truncate_to<F: AcirField>(input: &F, bits: u32) -> F {
 cfg_if::cfg_if! {
     if #[cfg(feature = "bls12_381")] {
         pub type FieldElement = field_element::FieldElement<ark_bls12_381::Fr>;
-    } else if #[cfg(feature = "secp256r1")] {
+    } else if #[cfg(feature = "p256")] {
         pub type FieldElement = field_element::FieldElement<ark_secp256r1::Fr>;
+    } else if #[cfg(feature = "t256")] {
+        pub type FieldElement = field_element::FieldElement<ark_tom256::Fr>;
     } else {
         pub type FieldElement = field_element::FieldElement<ark_bn254::Fr>;
     }
@@ -50,4 +52,4 @@ macro_rules! assert_unique_feature {
 }
 // https://internals.rust-lang.org/t/mutually-exclusive-feature-flags/8601/7
 // If another field/feature is added, we add it here too
-assert_unique_feature!("bn254", "bls12_381", "p256");
+assert_unique_feature!("bn254", "bls12_381", "p256", "t256");
