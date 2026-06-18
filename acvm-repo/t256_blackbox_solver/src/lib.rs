a9ba603b6602d36288a7eb41a9a330725bc193ce
Clement Humbert <clement.humbert@sicpa.com>
2026-06-18 07:44:16 +0200

    Remove 128 bits scalars constraints for T-256 blackbox

--- acvm-repo/t256_blackbox_solver/src/lib.rs
+++ acvm-repo/t256_blackbox_solver/src/lib.rs
@@ -3,10 +3,10 @@
 mod poseidon2;
 mod poseidon2_constants;
 
-use ark_ec::{AffineRepr, CurveGroup, VariableBaseMSM};
-use ark_ff::{BigInt, MontConfig, PrimeField, Zero};
 use acir::{AcirField, BlackBoxFunc};
 use acvm_blackbox_solver::{BlackBoxFunctionSolver, BlackBoxResolutionError};
+use ark_ec::{AffineRepr, CurveGroup, VariableBaseMSM};
+use ark_ff::{BigInt, MontConfig, PrimeField, Zero};
 
 type FieldElement = acir::acir_field::GenericFieldElement<ark_tom256::Fq>;
 
@@ -14,17 +14,14 @@ type FieldElement = acir::acir_field::GenericFieldElement<ark_tom256::Fq>;
 pub struct T256BlackboxSolver;
 
 impl T256BlackboxSolver {
-
     fn coordinates_to_affine(
         x: FieldElement,
         y: FieldElement,
     ) -> Result<ark_secp256r1::Affine, BlackBoxResolutionError> {
-        Ok(
-            ark_secp256r1::Affine::new_unchecked(
-                ark_secp256r1::Fq::from_bigint(x.into_repr().into_bigint()).unwrap(),
-                ark_secp256r1::Fq::from_bigint(y.into_repr().into_bigint()).unwrap(),
-            )
-        )
+        Ok(ark_secp256r1::Affine::new_unchecked(
+            ark_secp256r1::Fq::from_bigint(x.into_repr().into_bigint()).unwrap(),
+            ark_secp256r1::Fq::from_bigint(y.into_repr().into_bigint()).unwrap(),
+        ))
     }
 
     // Taken from the embedded_curve_ops in the Bn254 blackbox solver
@@ -32,7 +29,7 @@ impl T256BlackboxSolver {
         points: &[FieldElement],
         scalars_lo: &[FieldElement],
         scalars_hi: &[FieldElement],
-    ) -> Result<(Vec<ark_secp256r1::Affine>, Vec<BigInt<4>>), BlackBoxResolutionError>{
+    ) -> Result<(Vec<ark_secp256r1::Affine>, Vec<BigInt<4>>), BlackBoxResolutionError> {
         if points.len() != 2 * scalars_lo.len() || scalars_lo.len() != scalars_hi.len() {
             return Err(BlackBoxResolutionError::Failed(
                 BlackBoxFunc::MultiScalarMul,
@@ -45,32 +42,21 @@ impl T256BlackboxSolver {
         let mut big_ints = Vec::new();
 
         for i in (0..points.len()).step_by(2) {
-            let point =
-                Self::coordinates_to_affine(points[i], points[i + 1])
-                    .map_err(|e| BlackBoxResolutionError::Failed(BlackBoxFunc::MultiScalarMul, e.to_string()))?;
+            let point = Self::coordinates_to_affine(points[i], points[i + 1]).map_err(|e| {
+                BlackBoxResolutionError::Failed(BlackBoxFunc::MultiScalarMul, e.to_string())
+            })?;
 
-            let scalar_low: u128 =
-                T256BlackboxSolver::field_to_u128_limb(&scalars_lo[i / 2], BlackBoxFunc::MultiScalarMul)?;
+            // let scalar_high: u128 = T256BlackboxSolver::field_to_u128_limb(
+            //     &scalars_hi[i / 2],
+            //     BlackBoxFunc::MultiScalarMul,
+            // )?;
 
-            let scalar_high: u128 =
-                T256BlackboxSolver::field_to_u128_limb(&scalars_hi[i / 2], BlackBoxFunc::MultiScalarMul)?;
-
-            // Convert to BigInt<4>, using u64 limbs.
-            let limbs_array = [
-                scalar_low as u64,
-                (scalar_low >> 64) as u64,
-                scalar_high as u64,
-                (scalar_high >> 64) as u64,
-            ];
-            let scalar_bigint = BigInt::new(limbs_array);
+            let scalar_bigint = scalars_lo[0].into_repr().into_bigint();
 
             // Check if this is smaller than the P256 modulus
             if scalar_bigint >= ark_secp256r1::FrConfig::MODULUS {
                 // Format as hex string (big-endian, most significant limb first)
-                let hex_str = format!(
-                    "{:016x}{:016x}{:016x}{:016x}",
-                    limbs_array[3], limbs_array[2], limbs_array[1], limbs_array[0]
-                );
+                let hex_str = format!("{}", scalar_bigint);
                 return Err(BlackBoxResolutionError::Failed(
                     BlackBoxFunc::MultiScalarMul,
                     format!("{hex_str} is not a valid T256 scalar"),
@@ -102,25 +88,16 @@ impl BlackBoxFunctionSolver<FieldElement> for T256BlackboxSolver {
         points: &[FieldElement],
         scalars_lo: &[FieldElement],
         scalars_hi: &[FieldElement],
-        predicate: bool
+        predicate: bool,
     ) -> Result<(FieldElement, FieldElement), BlackBoxResolutionError> {
         if !predicate {
-            return Ok(
-                (
-                    FieldElement::zero(),
-                    FieldElement::zero(),
-                )
-            );
+            return Ok((FieldElement::zero(), FieldElement::zero()));
         }
 
         let (points, scalars) = Self::parse_msm_inputs(points, scalars_lo, scalars_hi)?;
 
-        let msm_result = ark_secp256r1::Affine::from(
-            ark_secp256r1::Projective::msm_bigint(
-                &points,
-                &scalars
-            )
-        );
+        let msm_result =
+            ark_secp256r1::Affine::from(ark_secp256r1::Projective::msm_bigint(&points, &scalars));
 
         if let Some((out_x, out_y)) = msm_result.xy() {
             Ok((
@@ -138,15 +115,10 @@ impl BlackBoxFunctionSolver<FieldElement> for T256BlackboxSolver {
         input1_y: &FieldElement,
         input2_x: &FieldElement,
         input2_y: &FieldElement,
-        predicate: bool
+        predicate: bool,
     ) -> Result<(FieldElement, FieldElement), BlackBoxResolutionError> {
         if !predicate {
-            return Ok(
-                (
-                    FieldElement::zero(),
-                    FieldElement::zero(),
-                )
-            );
+            return Ok((FieldElement::zero(), FieldElement::zero()));
         }
 
         let p1 = Self::coordinates_to_affine(*input1_x, *input1_y)?;
@@ -154,23 +126,23 @@ impl BlackBoxFunctionSolver<FieldElement> for T256BlackboxSolver {
 
         let sum = ark_secp256r1::Affine::from(p1 + p2);
         if let Some((x, y)) = sum.xy() {
-            Ok(
-                (
-                    FieldElement::from_repr(ark_tom256::Fq::from_bigint(x.into_bigint()).unwrap()),
-                    FieldElement::from_repr(ark_tom256::Fq::from_bigint(y.into_bigint()).unwrap()),
-                )
-            )
+            Ok((
+                FieldElement::from_repr(ark_tom256::Fq::from_bigint(x.into_bigint()).unwrap()),
+                FieldElement::from_repr(ark_tom256::Fq::from_bigint(y.into_bigint()).unwrap()),
+            ))
         } else {
             Ok((FieldElement::zero(), FieldElement::zero()))
         }
     }
 
-    fn poseidon2_permutation(&self, inputs: &[FieldElement]) -> Result<Vec<FieldElement>, BlackBoxResolutionError> {
+    fn poseidon2_permutation(
+        &self,
+        inputs: &[FieldElement],
+    ) -> Result<Vec<FieldElement>, BlackBoxResolutionError> {
         Ok(
             // TODO: this is obviously wrong and just here for the purpose of the comptime code from
             //     noir stdlib to compile
-            inputs.to_vec()
-            // poseidon2::poseidon2_permutation(inputs)?
+            inputs.to_vec(), // poseidon2::poseidon2_permutation(inputs)?
         )
     }
 }
